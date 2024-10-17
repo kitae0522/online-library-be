0ce1f88fd5f4e5c505bd3a99f64578d54189cf40
kitae0522 <kitae040522@gmail.com>
2024-10-17 23:14:39 +0900

    REFACTOR: Error Domain 분리

---
+++ pkg/domain/error.go
@@ -0,0 +1,11 @@
+package domain
+
+import "errors"
+
+var (
+	ErrIncorrectConfirmPassword = errors.New("incorrect confirm password")
+	ErrWrongPassword            = errors.New("wrong password")
+	ErrUnableToDeleteUser       = errors.New("unable to delete user")
+	ErrUnexpectedSigningMethod  = errors.New("unexpected signing method")
+	ErrInvalidTokenClaims       = errors.New("invalid token claims")
+)
--- pkg/utils/errorResponse.go
+++ pkg/utils/errorResponse.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"errors"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,10 +10,6 @@ type ErrorRes struct {
 	Message    string      `json:"message"`
 	Error      interface{} `json:"error"`
 }
-
-var ErrWrongPassword = errors.New("wrong password")
-var ErrUnexpectedSigningMethod = errors.New("unexpected signing method")
-var ErrInvalidTokenClaims = errors.New("invalid token claims")
 
 func CreateErrorRes(ctx *fiber.Ctx, statusCode int, errMessage string, err interface{}) error {
 	return ctx.Status(statusCode).JSON(ErrorRes{
