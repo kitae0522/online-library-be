62333cd37692462538206b5de6c905bcb2175acb
kitae0522 <kitae040522@gmail.com>
2024-10-18 23:54:12 +0900

    FIX: `CreateErrorRes` error 값이 제대로 안 담기는 오류

--- pkg/utils/errorResponse.go
+++ pkg/utils/errorResponse.go
@@ -12,10 +12,17 @@ type ErrorRes struct {
 }
 
 func CreateErrorRes(ctx *fiber.Ctx, statusCode int, errMessage string, err interface{}) error {
+	var errorDetail interface{}
+	if e, ok := err.(error); ok {
+		errorDetail = e.Error()
+	} else {
+		errorDetail = err
+	}
+
 	return ctx.Status(statusCode).JSON(ErrorRes{
 		IsError:    true,
 		StatusCode: statusCode,
 		Message:    errMessage,
-		Error:      err,
+		Error:      errorDetail,
 	})
 }
